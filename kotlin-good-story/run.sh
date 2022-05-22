ec54d3b06f9b42a0b7514c3587b7f6e98914286d
Joao Esperancinha <jofisaes@gmail.com>
2022-05-22 12:39:41 +0200

    🌿 Build target for Kotlin

--- kotlin-good-story/run.sh
+++ kotlin-good-story/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 java -version
-java -jar build/libs/kotlin-good-story.jar -h
-java -jar build/libs/kotlin-good-story.jar -f "$(git rev-parse --show-toplevel)"/docs/good.story/GoodStory.md \
+java -jar target/kotlin-good-story.jar -h
+java -jar target/kotlin-good-story.jar -f "$(git rev-parse --show-toplevel)"/docs/good.story/GoodStory.md \
                                           -lf "$(git rev-parse --show-toplevel)"/Log.md \
                                           -dump "$(git rev-parse --show-toplevel)"/dump
